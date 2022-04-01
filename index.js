37f51b2754eb3cdfc366f0c07b30f8f17fbbc800
NidhiSri1 <srinidhivaja@gmail.com>
2022-04-01 21:26:24 +0530

    c

--- index.js
+++ index.js
@@ -5,14 +5,27 @@ const port = process.env.PORT || 8000;
 const app = express();
 require("dotenv").config();
 const axios = require("axios");
-
+const client = require("twilio")(process.env.ACCOUNTSID, process.env.AUTHTOKEN);
 app.use(express.json());
 
-// app.get("/", (req, res) => {
-//     res.send("hi");
-// });
-
 app.use("/", ServiceControllers);
+
+//LOGIN
+
+app.post("/login", (req, res) => {
+    client.verify
+        .services(process.env.SERVICESID)
+        .verifications.create({
+            to: `+${918197023966}`,
+            channel: "sms",
+        })
+        .then((data) => {
+            res.status(200).send("data", verifications.status);
+        })
+        .catch((err) => {
+            console.log(err);
+        });
+});
 
 app.listen(port, async () => {
     try {
